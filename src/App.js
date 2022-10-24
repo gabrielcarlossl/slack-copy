83bcf358dd845644811e6dc4e5fa3dd92895ad89
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2022-10-24 10:42:19 -0300

    add favicon, removing unused files, created Home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="app">
-      <h1>Slack</h1>
+      <Router>
+        <>
+          <Routes>
+            <Route path="/" exact element={<Home></Home>}></Route>
+          </Routes>
+        </>
+      </Router>
     </div>
   );
 }
