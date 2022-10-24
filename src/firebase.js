ba3fe136d08191260c189af193067f08b7e3e6e8
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2022-10-24 09:45:46 -0300

    project init

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDXPNcSEoLbn9_Xmz2CBed81suYRkqN0hg",
+    authDomain: "slack-g7.firebaseapp.com",
+    projectId: "slack-g7",
+    storageBucket: "slack-g7.appspot.com",
+    messagingSenderId: "160792196648",
+    appId: "1:160792196648:web:70b9789a8dcfa76ccf271d"
+  };
