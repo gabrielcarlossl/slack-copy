713c1f01473d3a49a5c2bf20190e7bc3f157511f
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2022-10-25 14:17:41 -0300

    added integration with firebase

--- src/components/SideBarOptions.jsx
+++ src/components/SideBarOptions.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
+import { db } from '../firebase'
+import { addDoc, collection} from 'firebase/firestore'
 
 function SideBarOptions({ Icon, title, addChannelOption }) {
+
+    const addChannel =  async () => {
+        const channelName = prompt('Por favor diga o nome do canal:')
+
+        if (channelName) {
+            await addDoc(collection(db, 'rooms'),{
+                name: channelName,
+            })
+        }
+    }
+
+    const selectChannel = () => {}
+
     return (
-        <SideBarOptionsContainer>
+        <SideBarOptionsContainer onClick={addChannelOption ? addChannel : selectChannel}>
             {Icon && <Icon fontSize='small' style={{ padding: 10 }}></Icon>}
             {Icon ? (
                 <h3>{title}</h3>
--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp} from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDXPNcSEoLbn9_Xmz2CBed81suYRkqN0hg",
-    authDomain: "slack-g7.firebaseapp.com",
-    projectId: "slack-g7",
-    storageBucket: "slack-g7.appspot.com",
-    messagingSenderId: "160792196648",
-    appId: "1:160792196648:web:70b9789a8dcfa76ccf271d"
-  };
+  apiKey: "AIzaSyDXPNcSEoLbn9_Xmz2CBed81suYRkqN0hg",
+  authDomain: "slack-g7.firebaseapp.com",
+  projectId: "slack-g7",
+  storageBucket: "slack-g7.appspot.com",
+  messagingSenderId: "160792196648",
+  appId: "1:160792196648:web:70b9789a8dcfa76ccf271d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const storage = getStorage();
+
+export { app, db, storage };
